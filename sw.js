4173b9c01c96b035095c67e4c4916b2f135a1d3b
RBAK123 <anthonykosiuhang.ak@gmail.com>
2026-04-09 15:13:00 +0000

    Bump cache version to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vehicle-crm-v1';
+const CACHE = 'vehicle-crm-v2';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
