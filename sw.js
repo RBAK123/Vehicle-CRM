91e7bc2a6a601479372fa690e1429008b329b232
Vehicle CRM Deploy <deploy@vehicle-crm.app>
2026-04-11 07:04:58 +0000

    新增 MG M9 Premium / Luxury / Advan 車款

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vehicle-crm-v3';
+const CACHE = 'vehicle-crm-v4';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
